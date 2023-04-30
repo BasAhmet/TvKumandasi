7fc2910cb14e53e8468ba7531c2477703a54f938
Ahmet Baş <ahba8184@gmail.com>
2023-04-30 23:27:24 +0300

    30.04.2023

--- src/main/java/TvRemoteControl.java
+++ src/main/java/TvRemoteControl.java
@@ -1,3 +1,4 @@
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -9,15 +10,15 @@ Ses --> arttırma, azaltma, kapatma, açma
 kanal --> değiştirme (+ , -)
 kanal --> ekleme, listeleme
 kanala gitme --> kanal numarası girilecek
-kanal uydu bilgisi --> Kanal adı: .........
-                                    Uydu: ..................
-                                    Frekans: .............
-                                    Polarizasyon: .............
-                                    Symbol Rate: ...............
-                                    Fec: ....../......
+kanal uydu bilgisi -->  Kanal adı: .........
+                        Uydu: ..................
+                        Frekans: .............
+                        Polarizasyon: .............
+                        Symbol Rate: ...............
+                        Fec: ....../......
 
 ArrayList<String>[][] list = new ArrayList[][];
-ArrayList<ArrayList<Integer> > array_list = new ArrayList<ArrayList<Integer> >();
+ArrayList<ArrayList<String>> array_list = new ArrayList<ArrayList<String>>();
 
 [[kanal,uydu,frekans,polarizasyon,rate,fec],[kanal,uydu,frekans,polarizasyon,rate,fec],[kanal,uydu,frekans,polarizasyon,rate,fec]]
  */
@@ -26,6 +27,9 @@ public class TvRemoteControl {
         Scanner input = new Scanner(System.in);
         int ses = 3;
         ArrayList<String> kanallar = new ArrayList<>(Arrays.asList("TRT","ATV","AHABER","TV8","FOX","SHOW"));
+        String kanalBilgileri[][] = {{"TRT1","Türksat 4A","11958","V","27500","5/6"},
+                {"ATV","Türksat 4A","12053","H","27500","5/6"}};
+
         int kanal = 1;
         System.out.println("*********Kumanda Menüsü********\n"+
                 "Sesi Arttırma ve Azaltma --> 1\n" +
@@ -70,13 +74,47 @@ public class TvRemoteControl {
                     System.out.println("Yeni Kanal : " + kanallar.get(kanal-1));
                 }
             } else if (secenek.equals("3")) {
-                System.out.println("kanala gidiliyor");
+                int kanalNo = 1;
+                for (String w : kanallar ) {
+                    System.out.println(kanalNo + ". Kanal : " + w);
+                    kanalNo ++;
+                }
+                System.out.print("Gitmek istediğiniz kanalın numarasını giriniz : ");
+                int kanalNumarasi = input.nextInt();
+                kanal = kanalNumarasi;
+                System.out.println("Seçtiğiniz kanal : " + kanallar.get(kanal-1));
             } else if (secenek.equals("4")) {
-                System.out.println("kanal listesi");
-            } else if (secenek.equals("5")) {
+                System.out.println("************ Kanal Listesi ***********");
+                int kanalNo = 1;
+                for (String w : kanallar ) {
+                    System.out.println(kanalNo + ". Kanal : " + w);
+                    kanalNo ++;
+                }
+            }
+            /*
+                else if (secenek.equals("5")) {
+                System.out.println("*** Kanal Ekleme Sayfası ***");
+
+                System.out.print("Kanal Adı : ");
+                String kanalAdi = input.next();
+
+                System.out.print("Kanal Uydusu : ");
+                String kanalUydusu = input.next();
+
+
                 System.out.println("kanal ekleme");
-            } else if (secenek.equals("6")) {
-                System.out.println("kanal uydu bilgisi");
+            } */
+            else if (secenek.equals("6")) {
+                System.out.println("**** Kanal Biligileri ****");
+                for (int i = 0 ; i < kanalBilgileri.length ; i++){
+                    System.out.print("\nKanal Adı : " + kanalBilgileri[i][0]);
+                    System.out.print("\nUydu Adı : " + kanalBilgileri[i][1]);
+                    System.out.print("\nFrekans : " + kanalBilgileri[i][2]);
+                    System.out.print("\nPolarizasyon : " + kanalBilgileri[i][3]);
+                    System.out.print("\nSymbol : " + kanalBilgileri[i][4]);
+                    System.out.print("\nFec : " + kanalBilgileri[i][5]);
+                    System.out.println("\n**********************");
+                }
             } else if (secenek.equals("q")) {
                 System.out.println("Çıkış yapıldı");
                 break;
@@ -87,13 +125,4 @@ public class TvRemoteControl {
     }
 }
 
-//muhammet branch oluşturdum değişiklikler kendi branchımda yapıldı .
-///deneme yapiyorum stop
-//muhammet değişiklik
 
-// fatih degisiklik
-
-// Ali Degisiklik
-//sinem değişiklik
-//beyza değişiklik
-// fatih degisiklik
